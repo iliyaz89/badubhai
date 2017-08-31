df67b510e6628ea027eed0d28d6f0968f4580eff
Arun Reddy <arunreddy.nelakurthi@gmail.com>
2017-08-30 17:55:09 -0700

    Initial commit

---
+++ tp/app.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""[application description here]"""
+
+__appname__ = "Target Pricing Model"
+__author__ = "Iliaz"
+__version__ = "0.0pre0"
+__license__ = ""
+
+import logging
+
+log = logging.getLogger(__name__)
+
+import argparse
+import ConfigParser
+
+
+# -- Code Here --
+def set_up_opt_parser():
+    parser = argparse.ArgumentParser(description='TP 2.0 Engine.')
+    parser.add_argument('-v', '--verbose', type=int, default=2)
+    parser.add_argument('-q', '--quiet', type=int, default=0)
+
+    # ETL
+    parser.add_argument('-etl', '--run-etl', action='store_true', default=False)
+
+    args = parser.parse_args()
+
+    # Set up clean logging to stderr
+    log_levels = [logging.CRITICAL, logging.ERROR, logging.WARNING,
+                  logging.INFO, logging.DEBUG]
+
+    args.verbose = min(args.verbose - args.quiet, len(log_levels) - 1)
+    args.verbose = max(args.verbose, 0)
+    logging.basicConfig(level=log_levels[args.verbose],
+                        format='%(levelname)s: %(message)s')
+
+    return args
+
+
+def load_app_config():
+    config = ConfigParser.ConfigParser()
+    config.readfp(open('tp20_config.ini'))
+    return config
+
+
+if __name__ == '__main__':
+    args = set_up_opt_parser()
+    config = load_app_config()
+
+    if args.run_etl:
+        log.info('Running ETL script.')
+        log.debug('Debugging ETL script.')
+        log.error('Error in ETL script.')
+        log.critical('Critical in ETL script.')
+
